90ecfab6f4fe69d19204dcbbac376219e35ea2d8
mario-santoro89 <89361447+mario-santoro89@users.noreply.github.com>
2021-12-14 12:29:30 +0100

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-$('h3').click(function(e) {
-    $(e.target).next('div').siblings('div').slideUp();
-    $(e.target).next('div').slideToggle(); 
-    $(e.target).toggleClass('clicked');
-    $(e.target).siblings('h3').removeClass('clicked');
-    
-})
